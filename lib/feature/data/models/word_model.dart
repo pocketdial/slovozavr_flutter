055c6d177c88089019e437bf021af597d78f37b3
qmitry <qmitry@gmail.com>
2022-06-01 00:02:55 +0300

    add model, fix entity

---
+++ lib/feature/data/models/word_model.dart
@@ -0,0 +1,7 @@
+import 'package:slovozavr_flutter/feature/domain/entities/word_entity.dart';
+
+class WordModel extends WordEntity {
+  WordModel(String value) : super(value);
+
+  //method which return data?
+}
--- lib/feature/domain/entities/word_entity.dart
+++ lib/feature/domain/entities/word_entity.dart
@@ -1,10 +1,10 @@
 //import 'package:flutter/material.dart';
 
 class WordEntity {
-  String value;
+  final String value;
   //Color colour;
   //Color fontColour;
 
   //Word(this.value, this.colour, this.fontColour);
-  WordEntity(this.value);
+  const WordEntity(this.value);
 }
